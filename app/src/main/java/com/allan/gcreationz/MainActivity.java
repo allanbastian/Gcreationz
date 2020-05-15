331058a58691135a780d2b60511724712b795f3e
Allan Bastian <allan.bastian9.ab@gmail.com>
2020-05-15 16:59:51 +0530

    Added animation for logo

--- app/src/main/java/com/allan/gcreationz/MainActivity.java
+++ app/src/main/java/com/allan/gcreationz/MainActivity.java
@@ -2,13 +2,23 @@ package com.allan.gcreationz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = findViewById(R.id.logo);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in_anim);
+        logo.startAnimation(fadeIn);
     }
 }
