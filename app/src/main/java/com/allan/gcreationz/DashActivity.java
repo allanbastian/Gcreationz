b6f6f21071f98d46e13fbff5dc424f0e9659ca37
Allan Bastian <allan.bastian9.ab@gmail.com>
2020-05-19 08:22:05 +0530

    Added recycler for proflies

--- app/src/main/java/com/allan/gcreationz/DashActivity.java
+++ app/src/main/java/com/allan/gcreationz/DashActivity.java
@@ -1,14 +1,37 @@
 package com.allan.gcreationz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ProfileRecyclerAdapter adapter;
+    private Button addNewProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash);
+
+        recyclerView = findViewById(R.id.recycler);
+        addNewProfile = findViewById(R.id.add_new_profile_btn);
+        adapter = new ProfileRecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        addNewProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashActivity.this, NewProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
